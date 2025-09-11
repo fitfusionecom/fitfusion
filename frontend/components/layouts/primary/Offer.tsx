658528e3bb573aae11078d684e6bc14a775cf41d
deepakvishwakarma-hh <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2025-09-11 12:29:43 +0530

    Update offer code in Offer component: Change promotional code from FITFA50 to FITFA15 for accurate discount representation.

--- frontend/components/layouts/primary/Offer.tsx
+++ frontend/components/layouts/primary/Offer.tsx
@@ -10,7 +10,7 @@ const Offer: React.FC = () => {
       }}
     >
       <div className="offer-content">
-        <span style={{ color: "gold", fontWeight: "bold" }}>FITFA50</span>
+        <span style={{ color: "gold", fontWeight: "bold" }}>FITFA15</span>
         <span className="offer-text">
           Apply this code and get 15% off on your first order.
         </span>
