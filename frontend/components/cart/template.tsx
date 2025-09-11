cc5e1c4c608f4acdd672f299e68f49cb3e673b96
deepakvishwakarma-hh <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2025-09-11 12:43:05 +0530

    Refactor CartTemplate component: Remove breadcrumb section for a cleaner layout and improved user experience.

--- frontend/components/cart/template.tsx
+++ frontend/components/cart/template.tsx
@@ -12,26 +12,7 @@ const CartTemplate = ({
 }) => {
   return (
     <div>
-      <div className="ayur-bread-section">
-        <div className="ayur-breadcrumb-wrapper">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-12 col-md-12 col-sm-12">
-                <div className="ayur-bread-content">
-                  <h2>Cart</h2>
-                  <div className="ayur-bread-list">
-                    <span>
-                      <a href="/">Home</a>
-                    </span>
-                    <span className="ayur-active-page">Cart</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="ayur-bgcover ayur-cartpage-wrapper">
+      <div className="ayur-bgcover">
         <div className="container">
           <div className="row">
             <div className="col-lg-12 col-md-12 col-sm-12">
