3430ad0950479f671e6d8256fd1d872c2877dc0f
deepakvishwakarma-hh <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2025-10-25 12:46:56 +0530

    Update Facebook Pixel ID in layout.tsx: Change Pixel ID for tracking to the new ID for accurate event monitoring and update associated noscript tag.

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1130757668714361');
+            fbq('init', '1147089876841952');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -48,7 +48,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1130757668714361&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1147089876841952&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
